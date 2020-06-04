1de225f374304bc838273fd0b690b4be35c15801
notario <rafaelnotariorodriguez@gmail.com>
2020-06-04 00:21:23 -0500

    detallar ventanas internas -Listo Listo dscto percentage ambulantes 1. registro cliente -Listo 2. Nuevo producto -Listo  . apartado de gastos -Listo sesiones de uduario corte de caja -Listo
    4. Registro Ambulante- Listo
    5. Registro Cargador
    6. Apartado configuracion y creacion de tarifa
    4,5,6 -> Dedicar 15 hrs para detallar
    	-4hr

--- src/Interfaz/internos/jpanels/ambulantes.java
+++ src/Interfaz/internos/jpanels/ambulantes.java
@@ -6,6 +6,7 @@
 package Interfaz.internos.jpanels;
 
 
+import Interfaz.altaAmbulantes;
 import conexion.ConexionDBOriginal;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
@@ -36,14 +37,14 @@ public class ambulantes extends javax.swing.JPanel {
         
         cargaDatasAmbulantes();
                 
-        jTable1.getParent().addComponentListener(new ComponentAdapter() {
+        jTabViewAllAmbs.getParent().addComponentListener(new ComponentAdapter() {
     @Override
     public void componentResized(final ComponentEvent e) {
-        if (jTable1.getPreferredSize().width < jTable1.getParent().getWidth()) {
-            System.out.println("Resize"+jTable1.getParent().getWidth());
-            jTable1.setAutoResizeMode(jTable1.AUTO_RESIZE_ALL_COLUMNS);
+        if (jTabViewAllAmbs.getPreferredSize().width < jTabViewAllAmbs.getParent().getWidth()) {
+            System.out.println("Resize"+jTabViewAllAmbs.getParent().getWidth());
+            jTabViewAllAmbs.setAutoResizeMode(jTabViewAllAmbs.AUTO_RESIZE_ALL_COLUMNS);
         } else {
-            jTable1.setAutoResizeMode(jTable1.AUTO_RESIZE_OFF);
+            jTabViewAllAmbs.setAutoResizeMode(jTabViewAllAmbs.AUTO_RESIZE_OFF);
         }
     }
 });
@@ -53,7 +54,7 @@ public class ambulantes extends javax.swing.JPanel {
     
     protected void cargaDatasAmbulantes(){
             String[][] mat = matrizgetAmbsAll();
-             jTable1.setModel(new TModel(mat, cabAreasPays));        
+             jTabViewAllAmbs.setModel(new TModel(mat, cabAreasPays));        
     }
     
     //regresa matrizde vista tickets del dia
@@ -143,15 +144,31 @@ return mat;
         jTextField1 = new javax.swing.JTextField();
         jButton8 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jTabViewAllAmbs = new javax.swing.JTable();
+        jButton9 = new javax.swing.JButton();
 
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/informacion.png"))); // NOI18N
         jButton1.setText("Información");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/IconNewUser.png"))); // NOI18N
         jButton2.setText("Alta");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/abajo.png"))); // NOI18N
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Actualizar.png"))); // NOI18N
         jButton4.setText("Actualizar");
@@ -181,7 +198,7 @@ return mat;
 
         jButton8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/goma-de-borrar.png"))); // NOI18N
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jTabViewAllAmbs.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {"2", "ABEL BAUTISTA PALACIOS", "", null, null, null, "2.19 -16", "4", null},
                 {null, null, null, null, null, null, null, null, null},
@@ -192,31 +209,40 @@ return mat;
                 "No.", "Nombre", "Dirección", "Telefono", "Giro", "Tarifa", "Ult. Sem. Pag.", "Sem. Adeudo", "Venc. Inscripción"
             }
         ));
-        jTable1.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_ALL_COLUMNS);
-        jTable1.setMaximumSize(new java.awt.Dimension(2147483647, 1500));
-        jTable1.setRowHeight(22);
-        jTable1.setRowMargin(2);
-        jScrollPane1.setViewportView(jTable1);
-        if (jTable1.getColumnModel().getColumnCount() > 0) {
-            jTable1.getColumnModel().getColumn(0).setMinWidth(80);
-            jTable1.getColumnModel().getColumn(0).setPreferredWidth(80);
-            jTable1.getColumnModel().getColumn(0).setMaxWidth(80);
-            jTable1.getColumnModel().getColumn(3).setMinWidth(100);
-            jTable1.getColumnModel().getColumn(3).setPreferredWidth(150);
-            jTable1.getColumnModel().getColumn(3).setMaxWidth(300);
-            jTable1.getColumnModel().getColumn(4).setMinWidth(80);
-            jTable1.getColumnModel().getColumn(4).setPreferredWidth(120);
-            jTable1.getColumnModel().getColumn(4).setMaxWidth(250);
-            jTable1.getColumnModel().getColumn(6).setMinWidth(90);
-            jTable1.getColumnModel().getColumn(6).setPreferredWidth(140);
-            jTable1.getColumnModel().getColumn(6).setMaxWidth(250);
-            jTable1.getColumnModel().getColumn(7).setMinWidth(50);
-            jTable1.getColumnModel().getColumn(7).setPreferredWidth(80);
-            jTable1.getColumnModel().getColumn(7).setMaxWidth(150);
-            jTable1.getColumnModel().getColumn(8).setMinWidth(80);
-            jTable1.getColumnModel().getColumn(8).setPreferredWidth(120);
-            jTable1.getColumnModel().getColumn(8).setMaxWidth(500);
+        jTabViewAllAmbs.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_ALL_COLUMNS);
+        jTabViewAllAmbs.setMaximumSize(new java.awt.Dimension(2147483647, 1500));
+        jTabViewAllAmbs.setRowHeight(22);
+        jTabViewAllAmbs.setRowMargin(2);
+        jTabViewAllAmbs.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jTabViewAllAmbsMousePressed(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTabViewAllAmbs);
+        if (jTabViewAllAmbs.getColumnModel().getColumnCount() > 0) {
+            jTabViewAllAmbs.getColumnModel().getColumn(0).setMinWidth(80);
+            jTabViewAllAmbs.getColumnModel().getColumn(0).setPreferredWidth(80);
+            jTabViewAllAmbs.getColumnModel().getColumn(0).setMaxWidth(80);
+            jTabViewAllAmbs.getColumnModel().getColumn(3).setMinWidth(100);
+            jTabViewAllAmbs.getColumnModel().getColumn(3).setPreferredWidth(150);
+            jTabViewAllAmbs.getColumnModel().getColumn(3).setMaxWidth(300);
+            jTabViewAllAmbs.getColumnModel().getColumn(4).setMinWidth(80);
+            jTabViewAllAmbs.getColumnModel().getColumn(4).setPreferredWidth(120);
+            jTabViewAllAmbs.getColumnModel().getColumn(4).setMaxWidth(250);
+            jTabViewAllAmbs.getColumnModel().getColumn(6).setMinWidth(90);
+            jTabViewAllAmbs.getColumnModel().getColumn(6).setPreferredWidth(140);
+            jTabViewAllAmbs.getColumnModel().getColumn(6).setMaxWidth(250);
+            jTabViewAllAmbs.getColumnModel().getColumn(7).setMinWidth(50);
+            jTabViewAllAmbs.getColumnModel().getColumn(7).setPreferredWidth(80);
+            jTabViewAllAmbs.getColumnModel().getColumn(7).setMaxWidth(150);
+            jTabViewAllAmbs.getColumnModel().getColumn(8).setMinWidth(80);
+            jTabViewAllAmbs.getColumnModel().getColumn(8).setPreferredWidth(120);
+            jTabViewAllAmbs.getColumnModel().getColumn(8).setMaxWidth(500);
         }
+
+        jButton9.setBackground(new java.awt.Color(255, 51, 51));
+        jButton9.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jButton9.setText("Baja definitiva");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -225,7 +251,9 @@ return mat;
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1246, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1246, Short.MAX_VALUE)
+                        .addGap(110, 110, 110))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton1)
                         .addGap(2, 2, 2)
@@ -248,15 +276,17 @@ return mat;
                         .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 11, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(9, 9, 9)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jComboBox3, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(110, Short.MAX_VALUE))
+                        .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton9, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -273,17 +303,56 @@ return mat;
                     .addComponent(jSeparator1)
                     .addComponent(jSeparator2)
                     .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                    .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBox3)
-                    .addComponent(jTextField1)
                     .addComponent(jComboBox2)
                     .addComponent(jComboBox1)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jComboBox3)
+                    .addComponent(jTextField1)
+                    .addComponent(jButton8, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 560, Short.MAX_VALUE)
                 .addContainerGap(296, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        int fila = jTabViewAllAmbs.getSelectedRow();
+
+        if(fila > -1){
+            String varBus = jTabViewAllAmbs.getValueAt(fila, 0).toString();
+             altaAmbulantes altamb = new altaAmbulantes(varBus);
+            altamb.setVisible(true);
+            altamb.setEnabled(true);
+            altamb.validate();
+        }else{
+            JOptionPane.showMessageDialog(null,"no data View");
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTabViewAllAmbsMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTabViewAllAmbsMousePressed
+           if (evt.getClickCount() > 1) {
+                  jButton1.doClick();
+              }
+    }//GEN-LAST:event_jTabViewAllAmbsMousePressed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+             altaAmbulantes altamb = new altaAmbulantes("1");
+            altamb.setVisible(true);
+            altamb.setEnabled(true);
+            altamb.jButton2.doClick();
+            altamb.validate();        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        int fila = jTabViewAllAmbs.getSelectedRow();
+
+        if(fila > -1){
+            
+        }else{
+            JOptionPane.showMessageDialog(null,"no data View");
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -295,6 +364,7 @@ return mat;
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JComboBox<String> jComboBox3;
@@ -302,7 +372,7 @@ return mat;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTabViewAllAmbs;
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
